bc8a5feb55b4f7c261bc01c052f984904c8049a7
pedramcode <dev.dehghanpour@gmail.com>
2024-02-10 09:40:00 +0330

    default fixed node name

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 
 #include "settings.h"
 #include "network/tcp_server.h"
-#include "data/btree.h"
 
 static char doc[] = "Node Sync platform";
 const char *argp_program_version = "Node Sync 0.1.0";
@@ -40,25 +39,8 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state)
         }
         if (settings->node_name == NULL)
         {
-            struct timespec ts;
-            clock_gettime(CLOCK_REALTIME, &ts);
-            srand((unsigned int)ts.tv_nsec);
-            int random_number = rand() % 100000;
-            const char *PREFIX = "node_";
-            const int NUM_DIGITS = 5;
-
             // Allocate space for node_name
-            settings->node_name = malloc(sizeof(char) * (strlen(PREFIX) + NUM_DIGITS + 1));
-
-            if (settings->node_name == NULL)
-            {
-                fprintf(stderr, "Memory allocation failed for node_name\n");
-                free(settings);
-                return 1;
-            }
-
-            // Format the string with the prefix and random number
-            snprintf(settings->node_name, strlen(PREFIX) + NUM_DIGITS + 1, "%s%05d", PREFIX, random_number);
+            settings->node_name = "unnamed node";
         }
 
         if (has_error == 1)
@@ -90,14 +72,6 @@ void *__handler(void *conn_ptr)
 
 int main(int argc, char *argv[])
 {
-    // ========== Test ==========
-    btree_node_t *n1 = btree_init(typ_integer, 3);
-    btree_set_key(n1, 0, 10);
-    btree_set_key(n1, 1, 20);
-    btree_set_key(n1, 2, 30);
-
-    btree_free(n1);
-
     // ========== Server ==========
     settings_t settings;
     settings.node_name = NULL;
